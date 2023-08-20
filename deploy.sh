36884b9b5c1fe8ffd280878b4c63537747c7aad1
alirezajavid <navaro_81@yahoo.com>
2023-08-20 09:00:41 +0330

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+npm run build
 cp -r dist/* deply
 cd deply 
 git add .
@@ -6,5 +7,5 @@ git pull
 git push origin main
 cd ../dist
 tar czvf helix-ui.tar.gz  *
-scp -P 2022 helix-ui.tar.gz pi@103.solar.helixsec.live: 
-ssh pi@103.solar.helixsec.live -p 2022 'cd /opt/helix/solarhelix-service/templates/ && rm * -rf && tar xzvf ~/helix-ui.tar.gz '
+scp -P 2022 helix-ui.tar.gz pi@104.solar.helixsec.live: 
+ssh pi@104.solar.helixsec.live -p 2022 'cd /opt/helix/solarhelix-service/templates/ && rm * -rf && tar xzvf ~/helix-ui.tar.gz '
