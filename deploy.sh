5d6ee7d2bad216f2c6555e8bb0e88b97f4e6b9c7
alirezajavid <navaro_81@yahoo.com>
2023-09-02 07:04:23 +0330

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,4 @@ tar czvf helix-ui.tar.gz  *
 scp -P 2022 helix-ui.tar.gz pi@75.solar.helixsec.live:/tmp/
 ssh pi@75.solar.helixsec.live -p 2022 'cd /opt/helix/solarhelix-service/templates/ && rm * -rf && tar xzf /tmp/helix-ui.tar.gz '
 
+hel!xsec@123
